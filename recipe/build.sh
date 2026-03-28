308bb3f50d87e4ba81792df91687974af55144dd
Travers <traversc@users.noreply.github.com>
2026-03-27 19:26:34 -0700

    fix avx2 flag

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 export DISABLE_AUTOBREW=1
 
-# Conditionally apply AVX2 only for x86_64, not for ARM64
-if [[ "$(uname -m)" == "x86_64" ]]; then
-  SIMD_FLAG="--with-simd=AVX2"
-else
-  SIMD_FLAG=""
-fi
-
-${R} CMD INSTALL --configure-args="${SIMD_FLAG}" --build . ${R_ARGS}
+${R} CMD INSTALL --build . ${R_ARGS}
