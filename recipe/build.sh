9e591ef9c322a2ff696e1079c1b074da93d26382
Travers <traversc@users.noreply.github.com>
2026-03-28 00:53:23 -0700

    hardcode TBB flags for osx_arm

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 export DISABLE_AUTOBREW=1
 
-sed "s|\${R_HOME}|${CONDA_PREFIX}|g" src/Makevars.in > src/Makevars.in.tmp
-mv src/Makevars.in.tmp src/Makevars.in
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" == "1" ]]; then
+  # Avoid build-time R invocations when cross-compiling.
+  sed -i.bak \
+    -e 's|^PKG_CPPFLAGS=\(.*\)-DIS_UTF8_LOCALE=.*$|PKG_CPPFLAGS=\1-DIS_UTF8_LOCALE=1|' \
+    -e 's|^PKG_CXXFLAGS = .*RcppParallel::CxxFlags().*$|PKG_CXXFLAGS = -DTBB_INTERFACE_NEW @TBB_FLAG@|' \
+    -e 's|^PKG_LIBS = .*RcppParallel::RcppParallelLibs().*$|PKG_LIBS = -L. @COMPILER_SPECIFIC_LIBS@ @ZSTD_LIBS@ -ltbb -ltbbmalloc|' \
+    src/Makevars.in
+  rm -f src/Makevars.in.bak
+fi
 
 ${R} CMD INSTALL --build . ${R_ARGS}
