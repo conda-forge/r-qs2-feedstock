4071a28cbbc5acd2ba859f74b67de76c2592a694
Travers <traversc@users.noreply.github.com>
2026-03-27 23:48:16 -0700

    fix R_HOME lookup for osx-arm

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 export DISABLE_AUTOBREW=1
 
+sed "s|\${R_HOME}|${CONDA_PREFIX}|g" src/Makevars.in > src/Makevars.in.tmp
+mv src/Makevars.in.tmp src/Makevars.in
+
 ${R} CMD INSTALL --build . ${R_ARGS}
